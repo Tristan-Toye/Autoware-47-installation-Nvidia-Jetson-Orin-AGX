cf9dae203a4cf03f19d8cb2a604483a79be05f6a
Tristan Toye <toye01@imec.be>
2025-10-15 16:52:49 +0200

    nodes graph and list

--- commands.sh
+++ commands.sh
@@ -1,9 +1,9 @@
 
 # autoware with graphical interface
-ros2 launch autoware_launch logging_simulator.launch.xml map_path:=$HOME/autoware_map/sample-map-rosbag vehicle_model:=sample_vehicle sensor_model:=sample_sensor_kit rviz:=false
+rocds2 launch autoware_launch logging_simulator.launch.xml map_path:=$HOME/autoware_map/sample-map-rosbag vehicle_model:=sample_vehicle sensor_model:=sample_sensor_kit 
 
 #autoware without graphical interface
-ros2 launch autoware_launch logging_simulator.launch.xml map_path:=$HOME/autoware_map/sample-map-rosbag vehicle_model:=sample_vehicle sensor_model:=sample_sensor_kit
+ros2 launch autoware_launch logging_simulator.launch.xml map_path:=$HOME/autoware_map/sample-map-rosbag vehicle_model:=sample_vehicle sensor_model:=sample_sensor_kit rviz:=false
 
 
 #runnig rosbag replay
@@ -14,3 +14,14 @@ ros2 run rqt_graph rqt_graph
 
 #opeen rqt
 rqt
+
+#running nsys
+nsys profile \
+  --trace=cuda,osrt,nvtx \
+  --sample=cpu --cpuctxsw=process-tree \
+  -o test1_0910_1506 \
+  ros2 bag play ~/autoware_map/sample-rosbag/ -r 0.2 -s sqlite3
+  
+# running perf stats
+
+perf stat -o perf.out -- ros2 bag play ~/autoware_map/sample-rosbag/ -r 0.2 -s sqlite3
